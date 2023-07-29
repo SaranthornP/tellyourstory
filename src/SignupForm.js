93e49259e1361894ba281f9cf50228e15dda376d
Saranthorn0827 <64010827@kmitl.ac.th>
2023-07-29 13:06:08 +0700

    Change position of helmet setup

---
+++ src/SignupForm.js
@@ -0,0 +1,11 @@
+import { Helmet } from "react-helmet"
+
+export const SignupForm = () => {
+    return (
+        < Helmet>
+            <meta charSet="utf-8" />
+            <title>TellYourStory</title>
+            <link rel="canonical" href="http://www.learnbestcoding.com" />
+        </Helmet>
+    )
+}
